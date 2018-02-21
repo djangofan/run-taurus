aacf780aff23a5f3d033fb4c1bca9f794fefdbcb
Jon Austen <djangofan@gmail.com>
2018-02-20 22:30:42 -0800

    Minor edit. Added screenshot.

--- runTaurus.sh
+++ runTaurus.sh
@@ -1,8 +1,11 @@
-#!/bin/
+#!/bin/bash
 # script for OSX
 # set X display if you want:  https://fredrikaverpil.github.io/2016/07/31/docker-for-mac-and-gui-applications/
 # if xquart starts slow, delete: /opt/X11/var/run/font_cache.lock
 # use XMing X11 display on windows
+
+# if you correctly set XDisplay , and enable jmeter GUI in .yml,  then you will see both Jmeter
+#  start on your local machine and also a browser pointing to the test report.
 
 ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 xhost + $ip
@@ -14,3 +17,6 @@ docker run -i --rm -e DISPLAY=$ip:0 -v /tmp/.X11-unix:/tmp/.X11-unix -v $PWD:/bz
 #docker run -i --rm -v $PWD:/bzt-configs blazemeter/taurus testit.yml
 
 echo "Done"
+
+
+
